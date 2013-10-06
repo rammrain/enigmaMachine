de4f3ff989391d19c6a532e219faa9a3542e9f08
Rain Ramm <rammrain@gmail.com>
2013-10-06 21:25:11 +0300

    INITIAL COMMIT

---
+++ enigma.js
@@ -0,0 +1,27 @@
+Enigma = {
+	
+	settings: {
+		alphabet: 'ABCDEFGHIJKLMOPQRSTUVWXYZ',
+		rotors: {
+			'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+			'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+			'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+			'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+			'VZBRGITYUPSDNHLXAWMJQOFECK'
+		},
+		reflector: 'FVPJIAOYEDRZXWGCTKUQSBNMHL',
+		
+	},
+	
+	run: function () {
+		
+	},
+	
+	rotateWheel: function (n) {
+		
+	},
+	
+	getWheelValue: function (n) {
+		
+	},
+}
